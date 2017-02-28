f46b1707df095fdc626bd24901ceee69f91b6d49
luludust <elizabethduong96@gmail.com>
2017-02-27 23:18:53 -0800

    parameter check

--- search-player.php
+++ search-player.php
@@ -9,29 +9,33 @@ include_once "Player.php";
 
 	$searchTerm = $_GET['search'];
 
-	try {
-        $conn = new PDO("mysql:host=" . $this->DB_SERVER . ";dbname=" . $this->DB_NAME, $this->DB_USER, $this->DB_PASS);
-        // set the PDO error mode to exception
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	if (isset($searchTerm)) {
+		try {
+	        $conn = new PDO("mysql:host=" . $this->DB_SERVER . ";dbname=" . $this->DB_NAME, $this->DB_USER, $this->DB_PASS);
+	        // set the PDO error mode to exception
+	        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        $stmt = self::$conn->prepare("SELECT * FROM `2015-2016` WHERE Name=:name");
-        $name = strtolower(filter_var($searchTerm, FILTER_SANITIZE_STRING));
-        $stmt->bindParam(':name', $name);
-        $stmt->execute();
-        $stmt->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE,
-            'Player',
-            array('name', 'team', 'gp', 'min', 'fg_m', 'fg_a', 'fg_pct',
-                'three_pt_m', 'three_pt_a', 'three_pt_pct', 'ft_m', 'ft_a',
-                'ft_pct', 'reb_off', 'reb_def', 'reb_tot', 'ast', 'to', 'stl',
-                'blk', 'pf', 'ppg'));
-        $results = $stmt->fetchAll();
+	        $stmt = self::$conn->prepare("SELECT * FROM `2015-2016` WHERE Name=:name");
+	        $name = strtolower(filter_var($searchTerm, FILTER_SANITIZE_STRING));
+	        $stmt->bindParam(':name', $name);
+	        $stmt->execute();
+	        $stmt->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE,
+	            'Player',
+	            array('name', 'team', 'gp', 'min', 'fg_m', 'fg_a', 'fg_pct',
+	                'three_pt_m', 'three_pt_a', 'three_pt_pct', 'ft_m', 'ft_a',
+	                'ft_pct', 'reb_off', 'reb_def', 'reb_tot', 'ast', 'to', 'stl',
+	                'blk', 'pf', 'ppg'));
+	        $results = $stmt->fetchAll();
 
-        return json_encode($results);
+	        echo json_encode($results);
 
-        $stmt->closeCursor();
-        $conn = null;
+	        $stmt->closeCursor();
+	        $conn = null;
 
-    } catch (PDOException $e) {
-        return "error retrieving nba player stats";
-    }
+	    } catch (PDOException $e) {
+	        echo "error retrieving nba player stats";
+	    }
+	} else {
+		echo "no search term entered"
+	}
 ?>
